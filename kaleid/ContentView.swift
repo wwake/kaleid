d14d0df54cfcfa554d6a14ce773e2a6a893b81ac
Bill Wake <william.wake@acm.org>
2023-09-23 13:31:04 -0500

    Swift project

---
+++ kaleid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kaleid
+//
+//  Created by Bill Wake on 9/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kaleid/kaleidApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaleidApp.swift
+//  kaleid
+//
+//  Created by Bill Wake on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct kaleidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
