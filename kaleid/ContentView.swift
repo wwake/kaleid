17cc02edf8f0744ac0dcd7faf68f6d80553c72b2
Bill Wake <william.wake@acm.org>
2023-09-27 14:14:32 -0500

    Add gestures

--- kaleid/ContentView.swift
+++ kaleid/ContentView.swift
@@ -1,16 +1,39 @@
 import SwiftUI
 
 struct ContentView: View {
-  let xOffset = CGFloat(100)
-  let yOffset = CGFloat(-20)
+  @State private var location: CGPoint = CGPoint(x: 100, y: -20)
+  @State private var angle: Angle = .zero
+
+  var rotation: some Gesture {
+    RotateGesture()
+      .onChanged { value in
+        angle = value.rotation
+      }
+  }
+
+  var moveFocus: some Gesture {
+    DragGesture()
+      .onChanged { value in
+        self.location = CGPoint(
+          x: value.location.x / 2.0,
+          y: value.location.y / 4.0
+        )
+      }
+  }
 
   var body: some View {
-    KaleidView(count: 36) {
-      Image("demo")
-        .resizable()
-        .offset(x: xOffset, y: yOffset)
+    GeometryReader {geometry in
+
+      KaleidView(count: 6) {
+        Image("demo")
+          .resizable()
+          .offset(x: location.x, y: location.y)
+      }
+      .rotationEffect(angle)
+
+      .gesture(moveFocus)
+      .gesture(rotation)
     }
-    .padding()
   }
 }
 
