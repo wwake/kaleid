cf55ccf8d637c0ba3ab284ca3a6b1d79ad184a7c
Bill Wake <william.wake@acm.org>
2023-09-28 14:01:10 -0500

    Make Pie work in its own coordinates, rather than flipping and translating

--- kaleid/Pie.swift
+++ kaleid/Pie.swift
@@ -5,26 +5,23 @@ struct Pie: Shape {
 
   func path(in rect: CGRect) -> Path {
     let minDimension = min(rect.width, rect.height)
-
     let center = CGPoint(x: rect.midX, y: rect.midY)
 
     let angle = 2 * .pi / count
 
     var path = Path()
     path.move(to: center)
-    path.addLine(to: CGPoint(x: rect.midX, y: rect.midY + minDimension / 2.0))
+    path.addLine(to: CGPoint(x: rect.midX, y: rect.midY - minDimension / 2.0))
     path.addArc(
       center: center,
       radius: minDimension / 2.0,
-      startAngle: Angle.degrees(90),
-      endAngle: .degrees(90) - .radians(angle),
-      clockwise: true
+      startAngle: Angle.degrees(270),
+      endAngle: .degrees(270) + .radians(angle),
+      clockwise: false
     )
     path.closeSubpath()
 
     return path
-      .applying(CGAffineTransform(scaleX: 1.0, y: -1.0))
-      .applying(CGAffineTransform(translationX: 0.0, y: rect.height))
   }
 }
 
