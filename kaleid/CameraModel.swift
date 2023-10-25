3e20690fb7e6063240da3ea6b0da0185544147a7
Bill Wake <william.wake@acm.org>
2023-10-25 13:59:08 -0500

    Move CameraModel to its own file

---
+++ kaleid/CameraModel.swift
@@ -0,0 +1,93 @@
+import AVFoundation
+import UIKit
+
+public class CameraModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+  @Published public var error: String?
+  @Published public var image: UIImage?
+
+  private var captureSession: AVCaptureSession?
+  private var cameraQueue = DispatchQueue(label: "Camera queue")
+
+  override init() {
+    super.init()
+
+    getPermission()
+    self.captureSession = setupSession()
+
+    startSession()
+  }
+
+  func reportError(_ newText: String?) {
+    DispatchQueue.main.async {
+      self.error = newText
+    }
+  }
+
+  func getPermission() {
+    reportError(nil)
+    var allowedAccess = false
+    let blocker = DispatchGroup()
+    blocker.enter()
+    AVCaptureDevice.requestAccess(for: .video) { flag in
+      allowedAccess = flag
+      blocker.leave()
+    }
+    blocker.wait()
+
+    if !allowedAccess {
+      reportError("User did not allow access to camera")
+      return
+    }
+  }
+
+  func setupSession() -> AVCaptureSession? {
+    let session = AVCaptureSession()
+    session.beginConfiguration()
+
+    let videoDevice = AVCaptureDevice.default(for: .video)
+    guard videoDevice != nil, let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!), session.canAddInput(videoDeviceInput) else {
+      reportError("No camera detected")
+      return nil
+    }
+    session.addInput(videoDeviceInput)
+
+    let photoOutput = AVCaptureVideoDataOutput()
+    guard session.canAddOutput(photoOutput) else {
+      reportError("Can't add output images to camera session")
+      return nil
+    }
+    photoOutput.setSampleBufferDelegate(self, queue: cameraQueue)
+
+    session.addOutput(photoOutput)
+    session.commitConfiguration()
+
+    return session
+  }
+
+  public func captureOutput(
+    _ output: AVCaptureOutput,
+    didOutput sampleBuffer: CMSampleBuffer,
+    from connection: AVCaptureConnection
+  ) {
+    if let cvImage = sampleBuffer.imageBuffer {
+
+      // copy to jpegData so we don't retain video buffer
+      let resultImage = UIImage(ciImage: CIImage(cvPixelBuffer: cvImage))
+      let jpegData = resultImage.jpegData(compressionQuality: 0.85)
+
+      DispatchQueue.main.async {
+        self.image = UIImage(data: jpegData!)
+      }
+    }
+  }
+
+  func startSession() {
+    cameraQueue.async {
+      self.captureSession?.startRunning()
+    }
+  }
+
+  func stopSession() {
+    self.captureSession?.stopRunning()
+  }
+}
