8818938e5ca6d001f4e5d43ce4b9ff9e6d193e44
Bill Wake <william.wake@acm.org>
2023-10-30 13:03:54 -0500

    Put the camera tab first

--- kaleid/ContentView.swift
+++ kaleid/ContentView.swift
@@ -6,25 +6,25 @@ enum ActiveTab: Hashable {
 }
 
 struct ContentView: View {
-  @State private var activeTab = ActiveTab.photo
+  @State private var activeTab = ActiveTab.camera
   @StateObject private var camera = CameraModel()
   @State private var angle: Angle = .degrees(0)
 
   var body: some View {
     RotatedView(angle: $angle) {
       TabView(selection: $activeTab) {
-        PhotoKaleidoscope(angle: angle)
-          .tabItem {
-            Label("Photos", systemImage: "photo")
-          }
-          .tag(ActiveTab.photo)
-        
         CameraKaleidoscope(camera: camera)
           .tabItem {
             Label("Camera", systemImage: "camera")
           }
           .tag(ActiveTab.camera)
         
+        PhotoKaleidoscope(angle: angle)
+          .tabItem {
+            Label("Photos", systemImage: "photo")
+          }
+          .tag(ActiveTab.photo)
+
         ExperimentView(angle: angle)
           .tabItem {
             Label("Experiment", systemImage: "paperplane")
